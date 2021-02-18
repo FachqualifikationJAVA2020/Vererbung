0deaaad9f353f15f420ad8432f2fcd29213a097c
Nicole <43906055+Knotonium@users.noreply.github.com>
2021-02-18 12:05:40 +0100

    Add files via upload

---
+++ Nicole_Point2D.java
@@ -0,0 +1,43 @@
+package b.klassen;
+
+public class Nicole_Point2D {
+	private float x = 0.0f;
+	private float y = 0.0f;
+	
+	public Nicole_Point2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Nicole_Point2D () { }
+	
+	public void setX (float x) {
+		this.x = x;
+	}
+	
+	public void setY (float y) {
+		this.y = y;
+	}
+	
+	public void setXY (float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public float getX() {
+		return this.x;
+	}
+	
+	public float getY() {
+		return this.y;
+	}
+	
+	public float[] getXY() {
+		float[] xyArray = {this.getX(), this.getY()};
+		return xyArray;
+	}
+	
+	public String toString() {
+		return "(" + this.getX() + "," + this.getY() + ")";
+	}
+}
---
+++ Nicole_Point3D.java
@@ -0,0 +1,34 @@
+package b.klassen;
+
+public class Nicole_Point3D extends Nicole_Point2D{
+	
+	float z = 0.0f;
+	
+	public Nicole_Point3D(float x, float y, float z) {
+		
+	}
+	public Nicole_Point3D() { }
+
+	public float getZ() {
+		return this.z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+	public void setXYZ(float x, float y, float z) {
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
+
+			public float[] getXYZ() {
+				float[] ausgabe = { this.getX(),this.getY(), this.getZ() };
+					return ausgabe;
+			}
+			
+	public String toString() {
+		String ausgabe = "(" + this.getX() + "," + this.getY() + this.getZ() + ")";
+		return ausgabe;
+	}
+}
